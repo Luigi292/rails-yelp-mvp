bb6cd08fbaaa45b9fd2bc50fa2f0d506e494a8d9
Luigi Maretto <luigi.marets@gmail.com>
2024-05-01 13:59:32 +0100

    rails new with bootstrap & simple_form

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+# Create 5 restaurants with valid attributes
+
+Restaurant.create(name: 'Chinese Restaurant', address: '123 Hoe Street,London', phone_number: '123-456-7890', category: 'chinese')
+Restaurant.create(name: 'Italian Restaurant', address: 'Soho, London', phone_number: '234-567-8901', category: 'italian')
+Restaurant.create(name: 'Japanese Restaurant', address: '345, Camden Town, London', phone_number: '345-678-9012', category: 'japanese')
+Restaurant.create(name: 'French Restaurant', address: '678 Leicester Square, London', phone_number: '456-789-0123', category: 'french')
+Restaurant.create(name: 'Belgian Restaurant', address: '291 Walthamstow, London', phone_number: '567-890-1234', category: 'belgian')
